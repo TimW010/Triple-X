5cc76da6b348db2ba1a93d5f0ca0a739f0871219
TimW010 <0981877@hr.nl>
2021-12-20 16:55:27 +0100

    first steps c++

---
+++ TripleX.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::cout << "You are a Death Row inmate breaking out of the prison..." << std::endl;
+    std::cout << "You need to enter the correct codes to break out..." << std::endl;
+
+    int a = 4;
+    int b = 3;
+    int c = 8;
+
+    int sum = a + b + c;
+    int multiplication = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << multiplication;
+
+    return 0;
+}
